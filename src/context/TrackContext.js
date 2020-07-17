d5e25bf08457c91fbb348a247562ec120682d8e7
alelaru <alelaru@gmail.com>
2020-07-17 13:00:01 -0500

    Get the tracks

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -3,12 +3,19 @@ import trackerApi from "../api/tracker";
 
 const trackReducer = (state, action) => {
   switch (action.type) {
+    case "fetch_tracks":
+      // This will throw the info from the api
+      return action.payload;
     default:
       return state;
   }
 };
 
-const fetchTracks = (dispatch) => () => {};
+const fetchTracks = (dispatch) => async () => {
+  const response = await trackerApi.get("/tracks");
+  dispatch({ type: "fetch_tracjs", payload: response.data });
+};
+
 const createTrack = (dispatch) => async (name, locations) => {
   await trackerApi.post("/tracks", { name, locations });
   console.log("Hey im sending in them to the api", locations);
